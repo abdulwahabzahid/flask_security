b6623dd04d975d0304a90514b6df2249a14cbeaf
abdulwahabzahid <abdulwahab.zahid@questlab.pk>
2024-07-11 14:29:06 +0500

    8th commit

--- server/app.py
+++ server/app.py
@@ -98,5 +98,5 @@ def page_not_found(e):
 
 if __name__ == '__main__':
     # app.run(debug=True, host='0.0.0.0')
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True, host='0.0.0.0', port=10000)
 
